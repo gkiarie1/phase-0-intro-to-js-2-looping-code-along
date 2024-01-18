d70d006513c499ff713056de6e1c49ee706d4014
gkiarie1 <gkaris24@gmail.com>
2024-01-18 20:18:26 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(){
+    const name=["Guadalupe", "Ollie", "Aki"];
+    const eventName= "surprise";
+    let invitationMessages= [];
+    
+        for (let i = 0; i < name.length; i++) {
+            let message = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+            invitationMessages.push(message);
+          }
+          return invitationMessages;
+    }
+    
+    function countDown(){
+       let number=10;
+        while(number>=0){
+          console.log(number) 
+      number--;
+        }
+    }
+    
